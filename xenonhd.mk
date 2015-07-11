e60d35b6ed14a554a3742eb79479757ce3a85959
JustAkan <puppy1900@naver.com>
2015-07-11 15:43:13 +0900

    e980: xenonhd,pa build fix
    Signed-off-by: JustAkan <puppy1900@naver.com>

--- xenonhd.mk
+++ xenonhd.mk
@@ -3,11 +3,27 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common slim stuff.
-$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
-
 # Enhanced NFC
-$(call inherit-product, vendor/xenonhd/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/xenonhd/configs/nfc_enhanced.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/xenonhd/products/common.mk)
+
+# Telephony
+$(call inherit-product, vendor/xenonhd/configs/telephony.mk)
+
+# Inherit common build.prop overrides
+-include vendor/xenonhd/products/common_versions.mk
+
+# Inherit drm blobs
+-include vendor/xenonhd/products/common_drm.mk
+
+# Copy specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/xenonhd/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/xenonhd/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/xenonhd/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \ 
+    vendor/xenonhd/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
 # Inherit device configuration
 $(call inherit-product, device/lge/e980/full_e980.mk)
