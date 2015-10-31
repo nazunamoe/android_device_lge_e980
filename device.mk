acb3433ec85dedf38656f42a54000f3a8c34984f
Igor <ShadySquirrel@users.noreply.github.com>
2015-10-31 23:35:17 +0100

    Update additional bluetooth stuff from latest zombigrn's zip, build.prop part
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -106,7 +106,247 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.telephony.default_network=9 \
-	telephony.lteOnGsmDevice=1
+	telephony.lteOnGsmDevice=1 \
+	camera2.portability.force_api=1 \
+
+# Add pm.sleep_mode by popular request.
+PRODUCT_PROPERTY_OVERRIDES += \
+	pm.sleep_mode=1 \
+
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    dhcpcd.conf \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+        LiveWallpapers \
+        LiveWallpapersPicker \
+        VisualizationWallpapers \
+        librs_jni
+
+PRODUCT_COPY_FILES += \
+        device/lge/e980/init.gee-common.rc:root/init.gee-common.rc \
+        device/lge/e980/init.gee.usb.rc:root/init.gee.usb.rc
+
+PRODUCT_COPY_FILES += \
+	device/lge/e980/audio_policy.conf:system/etc/audio_policy.conf
+
+PRODUCT_COPY_FILES += \
+	device/lge/e980/qosmgr_rules.xml:system/etc/qosmgr_rules.xml
+
+PRODUCT_COPY_FILES += \
+	device/lge/e980/media_profiles.xml:system/etc/media_profiles.xml \
+	device/lge/e980/media_codecs.xml:system/etc/media_codecs.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+PRODUCT_COPY_FILES += \
+	device/lge/e980/kickstart_checker.sh:system/etc/kickstart_checker.sh
+
+# Prebuilt kl and kcm keymaps
+PRODUCT_COPY_FILES += \
+	device/lge/e980/hs_detect.kl:system/usr/keylayout/hs_detect.kl \
+	device/lge/e980/pmic8xxx_pwrkey.kl:system/usr/keylayout/pmic8xxx_pwrkey.kl \
+	device/lge/e980/hs_detect.kcm:system/usr/keychars/hs_detect.kcm \
+	device/lge/e980/pmic8xxx_pwrkey.kcm:system/usr/keychars/pmic8xxx_pwrkey.kcm
+
+# Prebuilt input device calibration files
+PRODUCT_COPY_FILES += \
+	device/lge/e980/touch_dev.idc:system/usr/idc/touch_dev.idc \
+	device/lge/e980/touch_dev.kl:system/usr/keylayout/touch_dev.kl
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+	frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+
+# GPS configuration
+PRODUCT_COPY_FILES += \
+	device/lge/e980/gps.conf:system/etc/gps.conf
+
+# NFC packages
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag \
+    com.android.nfc_extras
+
+# NFCEE access control
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    NFCEE_ACCESS_PATH := device/lge/e980/nfc/nfcee_access.xml
+else
+    NFCEE_ACCESS_PATH := device/lge/e980/nfc/nfcee_access_debug.xml
+endif
+
+# NFC access control + feature files + configuration
+PRODUCT_COPY_FILES += \
+    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=196608
+
+# Audio Configuration
+# FIXME: Remove persist.audio.handset.mic and persist.audio.fluence.mode
+#        while switching new audio HAL from legacy HAL
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.audio.handset.mic.type=digital \
+	persist.audio.dualmic.config=endfire \
+	persist.audio.fluence.voicecall=true \
+	persist.audio.handset.mic=dmic \
+	persist.audio.fluence.mode=endfire \
+	persist.audio.lowlatency.rec=false
+
+# Bluetooth additional config...
+PRODUCT_PROPERTY_OVERRIDES += \
+	bluetooth.chip.vendor=brcm \
+	ro.product.bluetooth=4.0 \
+	ro.bt.chipset=Broadcom BCM4334 \
+	ro.bt.version=4.0 \
+	ro.bt.stack=BTL-A \
+	ro.bt.stack.version=5.0 \
+	ro.bt.profiles=12806403 \
+	service.brcm.bt.activation=0 \
+	service.brcm.bt.srv_active=0 \
+	service.brcm.bt.hcid_active=0 \
+	service.brcm.bt.btld=0 \
+	service.brcm.bt.btld_pid=0 \
+	service.brcm.bt.avrcp_pass_thru=0 \
+	service.brcm.bt.avrcp_toggle=1 \
+	service.brcm.bt.btport_redir_on=1 \
+
+# Do not power down SIM card when modem is sent to Low Power Mode.
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.radio.apm_sim_not_pwdn=1
+
+# Ril sends only one RIL_UNSOL_CALL_RING, so set call_ring.multiple to false
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.telephony.call_ring.multiple=0
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.telephony.ril_class=LgeLteRIL \
+	ro.telephony.ril.config=qcomdsds,nodefaultdata
+
+# Upto 3 layers can go through overlays
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.hwc.mdpcomp.enable=true \
+
+# More overrides from stock build.prop
+PRODUCT_PROPERTY_OVERRIDES += \
+	debug.sf.hw=1 \
+	debug.egl.hw=1 \
+	debug.composition.type=gpu \
+
+PRODUCT_PACKAGES += \
+	librs_jni \
+	com.android.future.usb.accessory
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	e2fsck
+
+PRODUCT_PACKAGES += \
+	libgenlock \
+	liboverlay \
+	hwcomposer.msm8960 \
+	gralloc.msm8960 \
+	copybit.msm8960 \
+	memtrack.msm8960
+
+PRODUCT_PACKAGES += \
+	audio_policy.msm8960 \
+	audio.primary.msm8960 \
+	audio.a2dp.default \
+	audio.usb.default \
+	audio.r_submix.default \
+	libaudio-resampler
+
+# Voice processing
+PRODUCT_PACKAGES += libqcomvoiceprocessing
+PRODUCT_COPY_FILES += \
+    device/lge/e980/audio_effects.conf:system/vendor/etc/audio_effects.conf
+
+PRODUCT_PACKAGES += \
+        libmm-omxcore \
+	libdivxdrmdecrypt \
+	libOmxVdec \
+	libOmxVenc \
+	libOmxCore \
+	libstagefrighthw \
+	libc2dcolorconvert
+
+PRODUCT_PACKAGES += \
+	libloc_adapter \
+	libloc_eng \
+	libloc_api_v02 \
+	libgps.utils \
+	gps.msm8960
+
+# ShadySquirrel's edit: add & enable secure ADB.
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	rild.libpath=/system/lib/libril-qc-qmi-1.so \
+	ro.adb.secure=1 \
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	drm.service.enabled=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.interface=wlan0 \
+	wifi.supplicant_scan_interval=15
+
+# Enable AAC 5.1 output
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.aac_51_output_enabled=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+        debug.egl.recordable.rgba8888=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.qc.sensors.wl_dis=true \
+	ro.qualcomm.sensors.smd=true
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
+PRODUCT_COPY_FILES += \
+	device/lge/e980/fetch-swv:system/bin/fetch-swv
+
+# Try to build the kernel
+TARGET_KERNEL_SOURCE := kernel/lge/gproj
+TARGET_KERNEL_CONFIG := cyanogenmod_e980_defconfig
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := $(TARGET_KERNEL_SOURCE)
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+    
+# This is the mako-specific audio package
+$(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
 
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
