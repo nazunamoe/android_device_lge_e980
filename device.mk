cbc76e76e07dd6145b6855b548f3269631583590
Igor <ShadySquirrel@users.noreply.github.com>
2015-11-03 16:05:23 +0100

    build.prop: random additions and tweaks
    Change-Id: I0ac6706d097294c7755f003fd88a131fddc10d60

--- device.mk
+++ device.mk
@@ -322,7 +322,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	wifi.interface=wlan0 \
-	wifi.supplicant_scan_interval=15
+	wifi.supplicant_scan_interval=180
 
 # Enable AAC 5.1 output
 PRODUCT_PROPERTY_OVERRIDES += \
